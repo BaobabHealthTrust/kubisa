94e4c863e87cc907dd1d467039c60d4353aec622
Mwatha Bwanali <mwathabwanali@gmail.com>
2013-10-29 10:50:30 +0200

    init setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kubisa::Application.config.secret_token = 'f3efc6d92b47fe76add0ec2be37a8f7318c2a68202e17646557a22881136f81e2adb0736f8184f7d1073d9c91b63313f0b5990621c9761726db8cb11ab8f237a'
